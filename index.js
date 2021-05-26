e6e8ef31a29943cda042a51e4d1bba06ec84d20e
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-05-26 14:35:47 -0400

    adding updated index.js file

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//const titleCased = () => {
+// return tutorials
+//}
+
+
+function titleCase(string) {
+  return string.split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
+
+function titleCased() { 
+  return tutorials.map(tutorial => titleCase(tutorial));
+}
+
+
+
+//bascially taking all of those strings and mapping through them. When mapping through them you are 
+//splitting up all the words in the string and then mapping every word individually. This will be referencing
+//the first map..... by making the first letter upper case and then joining it up to the rest of the word
